496978ae369efa41492228a6db4bbc0dd9a89391
aidosgal <agalimzhan928@gmail.com>
2025-01-23 19:03:50 +0500

    feat(app): initial commit

---
+++ src/main.c
@@ -0,0 +1,35 @@
+/*
+ * Tasker - Personal Task Management Application
+ * 
+ * Copyright (c) 2024 Aidos "aidosgal" Galimzhan
+ * All Rights Reserved
+ * 
+ * This software is protected by copyright laws and international treaties.
+ * Unauthorized reproduction or distribution of this program, or any portion 
+ * of it, may result in severe civil and criminal penalties and will be 
+ * prosecuted to the maximum extent possible under law.
+ * 
+ * License: Proprietary/Commercial
+ * Author: Aidos "aidosgal" Galimzhan
+ * Version: 0.1.0
+ * Created: January 2024
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define APP_VERSION "0.1.0"
+#define APP_NAME "Tasker"
+
+int main(int argc, char *argv[]) {
+    printf("=== %s v%s ===\n", APP_NAME, APP_VERSION);
+    printf("Welcome to your personal task management application!\n");
+    
+    // TODO: Add argument parsing
+    // TODO: Initialize storage
+    // TODO: Main application loop
+
+    printf("Hello, World! Tasker is initializing...\n");
+
+    return 0;
+}
